1cb61df7cfb438a74b3d5ea64db8dc855c9f4a54
crotsu <crotsu@gmail.com>
2026-04-19 12:00:16 +0900

    Set chmod 755 on .py files and add chmod to install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ cp grader.py                  $INSTALL_DIR/grader.py
 cp scouter.py                 $INSTALL_DIR/scouter
 cp scouterPro.py              $INSTALL_DIR/scouterPro
 cp scouterExam.py             $INSTALL_DIR/scouterExam
+
+chmod 755 $INSTALL_DIR/yoroshikuonegaishima-su
+chmod 755 $INSTALL_DIR/grader.py
+chmod 755 $INSTALL_DIR/scouter
+chmod 755 $INSTALL_DIR/scouterPro
+chmod 755 $INSTALL_DIR/scouterExam
